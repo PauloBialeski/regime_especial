d9ca6e2dadab6d480ccc608a2469966e14302916
Gilberto Junior <56450453+juniorgilberto@users.noreply.github.com>
2023-10-14 17:11:46 -0400

    Mexendo com requisição

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
-let ra = document.querySelector("#ra");
-let nome = document.querySelector("#nome");
-let semestre = document.querySelector("#semestre");
-let periodo = document.querySelector("#periodo");
+let ra =  document.querySelector("#ra")
+let nome = document.querySelector("#nome")
+let contato = document.querySelector("#contato")
+let semestre =  document.querySelector("#semestre")
+let periodo = document.querySelector("#periodo")
 let form = document.querySelector("#form")
 
 form.addEventListener("submit", function(event){
@@ -10,19 +11,22 @@ form.addEventListener("submit", function(event){
     let dados = {
         ra: ra.value,
         nome: nome.value,
+        contato: contato.value,
         semestre: semestre.value,
         periodo: periodo.value
     };
-
-    fetch('https://regime-especial-default-rtdb.firebaseio.com/regimeespecial/regimes.json?auth=wZhwSeRHtyRJnrabzlBBpbfoPplj7BtXZ4tFUgAI', {
-        method: 'POST',
-        body: JSON.stringify(dados)
-    })
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(response){
-        alert("Suas informações foram enviadas!")
-    })
+        
 })
 
+
+var httpRequest;
+if (window.XMLHttpRequest) {
+    httpRequest = new XMLHttpRequest();
+} else if (window.ActiveXObject) {
+    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+httpRequest.open('GET', 'https://regime-especial-default-rtdb.firebaseio.com/regimeespecial.json?auth=wZhwSeRHtyRJnrabzlBBpbfoPplj7BtXZ4tFUgAI', true);
+
+httpRequest.setRequestHeader('Content-Type', 'application/json');
+httpRequest.send(dados);
