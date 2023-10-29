6214eacc6416cf29895178f8d1c910beb4fa4e5d
Gilberto Junior <56450453+juniorgilberto@users.noreply.github.com>
2023-10-29 17:08:07 -0400

    Disciplinas nos select

--- script/getcursos.js
+++ script/getcursos.js
@@ -11,20 +11,36 @@ async function getCursos() {
   let data = await response.json();
 
 
+
   data = Object.keys(data).map((array) => data[array]).map((curso) => {
     let option = document.createElement("option");
     option.setAttribute("value", `C${cursos.length}`);
     option.innerText = curso.nome
-
     cursos.appendChild(option);
   });
-}
-// async function getDisciplinas() {
-//   const response = await fetch("https://regime-especial-default-rtdb.firebaseio.com/regimeespecial/cursos/"+ cursoid +"disciplinas.json?auth=wZhwSeRHtyRJnrabzlBBpbfoPplj7BtXZ4tFUgAI");
+
   
-//   let data = await response.json();
+}
+async function getDisciplinas() {
+  
 
-//   console.log(data);
-// }
+  let response = await fetch("https://regime-especial-default-rtdb.firebaseio.com/regimeespecial/cursos.json?auth=wZhwSeRHtyRJnrabzlBBpbfoPplj7BtXZ4tFUgAI");
+  
+  let data = await response.json();
+  for(var id in data){
 
+    let urlDisc = "https://regime-especial-default-rtdb.firebaseio.com/regimeespecial/cursos/"+ id +"/disciplinas.json?auth=wZhwSeRHtyRJnrabzlBBpbfoPplj7BtXZ4tFUgAI"
+
+    let response = await fetch(urlDisc);
+    let urlDiscData = await response.json();
+    urlDiscData = Object.keys(urlDiscData).map((array) => urlDiscData[array]).map((disciplina) => {
+      let option = document.createElement("option");
+      option.setAttribute("value", `C${disciplinas.length}`);
+      option.innerText = disciplina.nome
+      disciplinas.appendChild(option);
+    });
+  }
+
+}
 getCursos();
+getDisciplinas();
