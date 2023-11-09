b8487fa90090d169831414ebc1bb4ccd9bbb5bce
Gilberto Junior <56450453+juniorgilberto@users.noreply.github.com>
2023-11-09 16:55:31 -0400

    Lista Regimes funcionando

--- script/getcursos.js
+++ script/getcursos.js
@@ -41,6 +41,54 @@ async function getDisciplinas() {
     
   }
 }
+
+function loadRegimes(_id_disciplina){    
+  console.log('loadRegimes');
+  $.ajax({
+      type:'GET',
+      url:'https://regime-especial-default-rtdb.firebaseio.com/regimeespecial/regimes.json?auth=wZhwSeRHtyRJnrabzlBBpbfoPplj7BtXZ4tFUgAI&orderBy="id_disciplina"&equalTo="'+_id_disciplina+'"',
+      contentType: "application/json",
+      crossDomain: true,
+      headers:{
+      
+      },        
+      success: function(data){                                    
+          $('#tb_regimes').append(
+              '<tr  class="tb_regimes_tr">'+
+              '<td class="tb_regimes_td"><b class="paragrafo_regimes">Nome da Disciplina</b></td>'+ 
+              '<td class="tb_regimes_td"><b class="paragrafo_regimes">Opções</b></td>'+                            
+              '</tr>'
+          );                   
+
+          Object.keys(data).forEach(
+              function(item){                                 
+                  $('#tb_regimes').append(
+                      '<tr class="tb_regimes_tr">'+
+                      '<td class="tb_regimes_tr_td">'+data[item].disciplina+'</td>'+ 
+                      '<td class="tb_regimes_tr_td"><button class="botao_regimes" id="'+item+'" onclick="ObterAlunosRegime('+"'"+item+"'"+')">Ver Alunos</button></td>'+
+                      '<td class="tb_regimes_tr_td"><button class="botao_regimes" onclick="InscreverRegime()">Me Inscrever</button></td>'+
+                      '</tr>'
+                  );                        
+              }
+          )
+      },
+      error: function(data){
+          console.log(data);            
+      }
+  })             
+}
+
+
+
+
+
 $(document).ready(
   getCursos()
 );
+
+$('#p_list_regimes').click(
+  function (){
+      var selectDisciplina = document.getElementById('disciplina'); 
+      loadRegimes(selectDisciplina.options[selectDisciplina.selectedIndex].value);
+  }    
+)
